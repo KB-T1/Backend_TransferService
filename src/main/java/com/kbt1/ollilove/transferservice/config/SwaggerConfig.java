0df9ebe07e9031a711c4af9485d0bb29125b4be3
Sehan, Kim <stanley710@naver.com>
2023-12-15 15:36:03 +0900

    feat: Swagger 세팅 완료 - :18081/swagger-ui/index.html

---
+++ src/main/java/com/kbt1/ollilove/transferservice/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.kbt1.ollilove.transferservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "TransferService API 명세서",
+                description = "올리사랑 서비스 API 명세서", version = "v1"))
+
+@Configuration
+public class SwaggerConfig {
+
+}
