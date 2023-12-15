fa9164943798787fc5732f43fb195eb449920989
Sehan, Kim <stanley710@naver.com>
2023-12-15 14:21:45 +0900

    feat: application yaml 파일 설정, DB 연결

---
+++ src/test/java/com/kbt1/ollilove/transferservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbt1.ollilove.transferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
