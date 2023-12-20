6c1aae2758ab99aa826f59cefc50de232f664041
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2023-12-20 16:52:37 +0900

    fix : cors 오류 고치는중

--- src/main/java/com/kbt1/ollilove/transferservice/config/SpringConfig.java
+++ src/main/java/com/kbt1/ollilove/transferservice/config/SpringConfig.java
@@ -1,10 +1,10 @@
-package com.kbt1.ollilove.transferservice.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SpringConfig {
-
-}
+//package com.kbt1.ollilove.transferservice.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SpringConfig {
+//
+//}
