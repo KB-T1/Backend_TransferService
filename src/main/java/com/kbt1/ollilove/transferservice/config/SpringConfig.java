4b002826557dffd5b15f3c6a98f4cca1343cd7a1
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2023-12-20 17:14:02 +0900

    cors

--- src/main/java/com/kbt1/ollilove/transferservice/config/SpringConfig.java
+++ src/main/java/com/kbt1/ollilove/transferservice/config/SpringConfig.java
@@ -1,9 +1,9 @@
-package com.kbt1.ollilove.transferservice.config;
-
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SpringConfig {
-
-}
+//package com.kbt1.ollilove.transferservice.config;
+//
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SpringConfig {
+//
+//}
